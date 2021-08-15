b1dbf7b13800192fd78eaa8efb8426adbeb7ec0a
Alex <alex.65.santanna@gmail.com>
2021-08-14 22:53:14 -0300

    arrumando todos os warning

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,16 +4,24 @@ import linkedin from "../../images/linkedin-in-brands.svg";
 import youtube from "../../images/youtube-brands.svg";
 import github from "../../images/icone_github.svg";
 
-export default function Footer(){
-  return(
+export default function Footer() {
+  return (
     <footer className="rodape">
-      <h2 className="rodape__texto"> Copyrights&copy; Autor: <a href="https://alexricc2.github.io/" className="link">Alex Sant'Anna</a></h2> 
-    <div>
-        <a href="https://www.instagram.com/alex_ricardu/" target="_blank"><img src={instagram} className="rodape__icones"/></a>
-        <a href="https://www.linkedin.com/in/alex-ricardo-rodrigues-sant-anna/" target="_blank" ><img src={linkedin} className="rodape__icones"/></a>
-        <a href="https://www.youtube.com/channel/UCubvUoQdw0_RSv8P3Rgfekg" target="_blank" ><img src={youtube} className="rodape__icones"/></a>
-        <a href="https://github.com/alexRicc2" target="_blank" ><img src={github} className="rodape__icones"/></a>
-    </div>
+      <h2 className="rodape__texto"> Copyrights&copy; Autor: <a href="https://alexricc2.github.io/" className="link">Alex Sant'Anna</a></h2>
+      <div>
+        <a href="https://www.instagram.com/alex_ricardu/" target="_blank" rel="noreferrer">
+          <img src={instagram} className="rodape__icones" alt="instagram do criador"/>
+        </a>
+        <a href="https://www.linkedin.com/in/alex-ricardo-rodrigues-sant-anna/" target="_blank" rel="noreferrer">
+          <img src={linkedin} className="rodape__icones" alt="linkedin do criador"/>
+        </a>
+        <a href="https://www.youtube.com/channel/UCubvUoQdw0_RSv8P3Rgfekg" target="_blank" rel="noreferrer">
+          <img src={youtube} className="rodape__icones" alt="youtube do criador"/>
+        </a>
+        <a href="https://github.com/alexRicc2" target="_blank" rel="noreferrer">
+          <img src={github} className="rodape__icones" alt="github do criador"/>  
+        </a>
+      </div>
     </footer>
   )
 }
--- src/components/FormLogin/index.js
+++ src/components/FormLogin/index.js
@@ -1,39 +1,43 @@
 import React from 'react';
 import google from '../../images/icone_google.png';
 import facebook from '../../images/icone_facebook.png';
-export default function FormLogin(){
-  return(
+export default function FormLogin() {
+  return (
     <form className="form" id="matricula">
-        <span className="form__titulo">Já sou Aluno</span>
-        <div className="form__input">
-            <input type="email" name="email" className="form__input__input" placeholder="Email" data-form-email/>
-        </div>
+      <span className="form__titulo">Já sou Aluno</span>
+      <div className="form__input">
+        <input type="email" name="email" className="form__input__input" placeholder="Email" data-form-email />
+      </div>
 
-        <div className="form__input">
-            <input type="password" name="password" className="form__input__input" placeholder="Password" data-form-senha/>
+      <div className="form__input">
+        <input type="password" name="password" className="form__input__input" placeholder="Password" data-form-senha />
+      </div>
+      <div className="form__secao">
+        <div>
+          <input type="checkbox" id="ckb" />
+          <label for="ckb">Lembrar de mim</label>
         </div>
-        <div className="form__secao">
-            <div>
-                <input type="checkbox" id="ckb"/>
-                <label for="ckb">Lembrar de mim</label>
-            </div>
-            <div>
-              <a href="#" className="form__esqueci">Esqueci a senha</a>  
-            </div>
+        <div>
+          <a href="./index.html" className="form__esqueci">Esqueci a senha</a>
         </div>
-        <div className="form__btn">
-            <button className="form__login" data-form-botao>
-                Login
-            </button>
-        </div>
-        <div className="form__cadastro">
-            <span>Ou cadastre-se usando</span>
-        </div>
+      </div>
+      <div className="form__btn">
+        <button className="form__login" data-form-botao>
+          Login
+        </button>
+      </div>
+      <div className="form__cadastro">
+        <span>Ou cadastre-se usando</span>
+      </div>
 
-        <div className="form__redes">
-            <a href="#" className="cadastro__icones"><img src={google} style={{width: "24px"}}/></a>
-            <a href="#"><img src={facebook}/></a>
-        </div>
+      <div className="form__redes">
+        <a href="./index.html" className="cadastro__icones">
+          <img src={google} style={{ width: "24px" }} alt="logar com google"/>
+        </a>
+        <a href="./index.html">
+          <img src={facebook} alt="logar com facebook"/>
+        </a>
+      </div>
     </form>
   )
 }
